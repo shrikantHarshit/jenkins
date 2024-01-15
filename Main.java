d39b71357b85faaafbb23ae4cb0191ee7fab04eb
shrikantHarshit <154224088+shrikantHarshit@users.noreply.github.com>
2024-01-15 07:54:04 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+
+    public static void main(String args[]){
+        System.out.println("This is java program");
+        System.out.println("Git - jenkins ");
+        System.out.println(new Date());
+
+
+    }
+}
